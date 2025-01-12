9b22dc0f389309deb340a79a36ee985960414b5c
shira1-code <shirabodek12@gmail.com>
2025-01-12 10:03:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ df = df.dropna()
 # Create input and output datasets
 X = df[input_features]
 y = df[output_feature]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 scaler = MinMaxScaler()
 X_scaled = scaler.fit_transform(X) 
